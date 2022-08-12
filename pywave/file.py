907f2c3132bd02add1f3377265074df4156d2085
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-08-13 00:02:09 +0100

    Added write support and started change audio speed

--- pywave/file.py
+++ pywave/file.py
@@ -7,25 +7,35 @@ This module allows WAV files to be read and saved.
 """
 
 
-
 def read(file_location: str) -> data.WaveData:
     """
     Reads a WAV file from a given file location.
+    Currently, only 8-bit and 16-bit WAV files are supported.
 
     Returns a WaveData object.
     """
     with wave.open(file_location, "rb") as f:
         frames = f.readframes(f.getnframes())
-
         sample_rate = f.getframerate()
         bit_depth = f.getsampwidth() * 8 # Bytes to bits.
         channels = f.getnchannels()
 
-        metadata = data.WaveMetadata(sample_rate, bit_depth, channels)
+    metadata = data.WaveMetadata(sample_rate, bit_depth, channels)
 
-        return data.WaveData(frames, metadata)
+    return data.WaveData(frames, metadata)
 
 
-info = read("cna.wav")
-print(info.info.get_bitrate())
-    
+def write(wave_data: data.WaveData, file_location: str) -> None:
+    """
+    Writes WAV data to a given file.
+
+    Warning: Existing file will be overwritten.
+    """
+    with wave.open(file_location, "wb") as f:
+        f.setframerate(wave_data.info.sample_rate)
+        f.setsampwidth(wave_data.info.bit_depth // 8) # Bits to bytes.
+        f.setnchannels(wave_data.info.channels)
+        f.writeframes(wave_data.data)
+
+
+save = write # Alias
